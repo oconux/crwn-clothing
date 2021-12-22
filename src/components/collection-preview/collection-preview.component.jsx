e6614426e20f32f10b184ecf8efd30edc897e075
oconux <tony.ocon@oconfamily.net>
2021-12-22 13:19:19 -0600

    update code
    update code

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
@@ -7,8 +8,9 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {
-        items.filter( (item, idx) => idx < 4 ).map(item =>(
-          <div key={item.id}>{item.name} | ${item.price}.00</div>
+        items.filter( (item, idx) => idx < 4 )
+        .map(({ id, ...otherItemProps }) =>(
+          <CollectionItem key={id} {...otherItemProps} ></CollectionItem>
         ))
       }
     </div>
