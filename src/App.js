3440482d31841eecf45ebeb86f661d200bcf55b2
oconux <tony.ocon@oconfamily.net>
2021-12-22 16:51:31 -0600

    update
    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Link as RouterLink } from 'react-router-dom';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -25,6 +26,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<><HomePage /></>} />
         <Route exact path="/shop" element={<><ShopPage /></>} />
+        <Route exact path="/signin" element={<><SignInAndSignUpPage /></>} />
       </Routes>
 
       </div>
