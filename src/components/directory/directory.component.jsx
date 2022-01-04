f8329c695fa1805381f8882ffa193826591cf3ac
oconux <tony.ocon@oconfamily.net>
2022-01-04 10:59:45 -0600

    update spelling
    update spelling

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,13 +9,13 @@ class Directory extends React.Component {
     this.state = {
       sections : [
         {
-          title: 'hatssss',
+          title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
           linkUrl: 'shop/hats'
         },
         {
-          title: 'jacketss',
+          title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
           linkUrl: 'shop/jackets'
