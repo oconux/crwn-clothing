efd5ffa20f7d732b20346f553e6ade88d624f18e
oconux <tony.ocon@oconfamily.net>
2022-01-28 16:02:55 -0600

    updated data
    updated data

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,35 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    cartItem => cartItem.id == cartItemToAdd.id
+    cartItem => cartItem.id === cartItemToAdd.id
   );
 
   if(existingCartItem) {
     return cartItems.map(cartItem =>
-      cartItem.id == cartItemToAdd.id
+      cartItem.id === cartItemToAdd.id
       ? { ...cartItem, quantity: cartItem.quantity + 1}
       : cartItem
     )
   }
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+}
+
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  let qty = 1;
+
+  if(existingCartItem.quantity === 0) {
+    //return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id)
+    qty = 0;
+  }
+
+  return cartItems.map(
+    cartItem =>
+    cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - qty }
+    : cartItem
+  )
 }
